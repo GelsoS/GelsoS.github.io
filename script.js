17dcbae22ab561c0642c3061d5580cc420d2e382
GelsoS <gelso02@live.com>
2024-09-29 16:52:57 -0300

    add video de projeto

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ function toggleFullscreen(img) {
   img.classList.toggle("fullscreen");
   console.log(img);
 }
+
+const video = document.getElementById('myVideo');
+window.addEventListener('scroll', () => {
+  const rect = video.getBoundingClientRect();
+  if (rect.top + rect.height < 0 || rect.bottom > window.innerHeight) {
+    video.requestPictureInPicture();
+    console.table({ 'rect': rect, 'window': window.innerHeight });
+  } else {
+    // Remove o Picture-in-Picture se o vídeo estiver visível
+    if (document.pictureInPictureElement === video) {
+      document.exitPictureInPicture();
+      console.log("desativado");
+    }
+  }
+});
